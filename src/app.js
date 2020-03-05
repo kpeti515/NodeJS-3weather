47db8d5083297c09c235644cc0b063ab75311c12
Lorand Kallai <lorandkallai@gmail.com>
2020-03-05 08:54:55 +0100

    Remove console.log call + Change name

--- src/app.js
+++ src/app.js
@@ -21,14 +21,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather',
-    name: 'Andrew Mead'
+    name: 'Lorand Kallai'
   })
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Me',
-    name: 'Andrew Mead'
+    name: 'Lorand Kallai'
   })
 })
 
@@ -36,7 +36,7 @@ app.get('/help', (req, res) => {
   res.render('help', {
     helpText: 'This is some helpful text.',
     title: 'Help',
-    name: 'Andrew Mead'
+    name: 'Lorand Kallai'
   })
 })
 
@@ -73,7 +73,6 @@ app.get('/products', (req, res) => {
     })
   }
 
-  console.log(req.query.search)
   res.send({
     products: []
   })
@@ -82,7 +81,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
   res.render('404', {
     title: '404',
-    name: 'Andrew Mead',
+    name: 'Lorand Kallai',
     errorMessage: 'Help article not found.'
   })
 })
@@ -90,7 +89,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
   res.render('404', {
     title: '404',
-    name: 'Andrew Mead',
+    name: 'Lorand Kallai',
     errorMessage: 'Page not found.'
   })
 })
