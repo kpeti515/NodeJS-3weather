3833532552370127874248ca830a48346746b7aa
Lorand Kallai <lorandkallai@gmail.com>
2020-03-05 12:39:13 +0100

    added max-min temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,11 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location-forecast', undefined)
     } else {
-      callback(undefined, body.daily.data[0].summary + ' Jelenleg ' + body.currently.temperature + ' Celsius fok van. Esély az esőre: ' + body.currently.precipProbability + '%.')
+      callback(undefined, body.daily.data[0].summary + '\
+       Jelenleg' + body.currently.temperature + ' Celsius fok van. \
+       Esély az esőre: ' + body.currently.precipProbability + '%.  \
+       Ma várható maximum hőmérséklet: ' + body.daily.data[0].temperatureHigh + 'Celsius fok. \
+       A várható minimum: ' + body.daily.data[0].temperatureLow + ' Celsius fok.')
     }
   })
 }
